3ed4885b93cda5de611d3b1ee957dca97a7c872b
Prmeher Kaur <prmeherkaur@gmail.com>
2021-03-16 15:10:31 +0530

    Layout for LOCAL-API and CLI set out and linked to application

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import path from 'path';
+import { createCellsRouter } from './routes/cells';
 export const serve = (port: number, filename: String, dir: string, useProxy: boolean) => {
 	const app = express();
 	const packagePath = require.resolve('local-client/build/index.html');
@@ -13,6 +14,9 @@ export const serve = (port: number, filename: String, dir: string, useProxy: boo
 				})
 			)
 		: app.use(express.static(path.dirname(packagePath)));
+
+	app.use(createCellsRouter(filename, dir));
+
 	return new Promise<void>((resolve, reject) => {
 		app.listen(port, resolve).on('error', reject);
 	});
